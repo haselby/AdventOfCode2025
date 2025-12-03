fe046ba80f4de2015439f75df5e88c1f2fb01822
Haselby <jeffrey.haselby@hp.com>
2025-12-02 22:04:49 -0700

    Added puzzle input plus, updated Part1Solver, solved Day1Part1 puzzle

--- src/main/java/aoc/day01/Part1Solver.java
+++ src/main/java/aoc/day01/Part1Solver.java
@@ -1,6 +1,7 @@
 package aoc.day01;
 
 import aoc.Solver;
+import java.util.List;
 
 /**
  * Solver for Day 1, Part 1 of Advent of Code 2025.
@@ -10,8 +11,18 @@ public class Part1Solver implements Solver {
     @Override
     public String solve(String input) {
         try {
-            // TODO: Implement solution for Day 1, Part 1
-            return "Not yet implemented";
+            //Solution for Day 1, Part 1
+            List<String> sequenceOfRotations = input.lines().toList();
+
+            CombinationLock safe = new CombinationLock(50);
+            for (String rotation : sequenceOfRotations) {
+                safe.rotate(rotation);
+            }
+
+            int answer = safe.getPreciselyZeroCounter();
+
+            
+            return String.valueOf(answer);
         } catch (Exception e) {
             return "Error: " + e.getMessage();
         }
